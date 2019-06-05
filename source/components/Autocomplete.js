d4ebffb25065d56a6e69d6e8178f7324493967da
volleywebocean <volleysport1234@outlook.com>
2019-06-05 14:38:18 -0400

    add files

---
+++ source/components/Autocomplete.js
@@ -0,0 +1,320 @@
+// @flow
+import React, { Component } from 'react';
+import type { ComponentType, Element } from 'react';
+
+// external libraries
+import createRef from 'create-react-ref/lib/createRef';
+import _ from 'lodash';
+
+// interal components
+import { GlobalListeners } from './HOC/GlobalListeners';
+
+// internal utility functions
+import { createEmptyContext, withTheme } from './HOC/withTheme';
+import { composeTheme, addThemeId, didThemePropsChange } from '../utils/themes';
+import { composeFunctions } from '../utils/props';
+
+import { IDENTIFIERS } from '.';
+import type { ThemeContextProp } from './HOC/withTheme';
+
+type Props = {
+  className?: string,
+  context: ThemeContextProp,
+  error: ?string,
+  invalidCharsRegex: RegExp,
+  isOpeningUpward: boolean,
+  label?: string | Element<any>,
+  maxSelections?: number,
+  maxVisibleOptions: number,
+  multipleSameSelections: boolean,
+  onChange?: Function,
+  options: Array<any>,
+  preselectedOptions?: Array<any>,
+  placeholder?: string,
+  renderSelections?: Function,
+  renderOptions?: Function,
+  skin?: ComponentType<any>,
+  sortAlphabetically: boolean,
+  theme: ?Object, // will take precedence over theme in context if passed
+  themeId: string,
+  themeOverrides: Object
+};
+
+type State = {
+  inputValue: string,
+  error: string,
+  selectedOptions: Array<any>,
+  filteredOptions: Array<any>,
+  isOpen: boolean,
+  composedTheme: Object
+};
+
+class AutocompleteBase extends Component<Props, State> {
+  // declare ref types
+  rootElement: ?Element<any>;
+  inputElement: ?Element<'input'>;
+  suggestionsElement: ?Element<any>;
+  optionsElement: ?Element<any>;
+
+  // define static properties
+  static displayName = 'Autocomplete';
+  static defaultProps = {
+    context: createEmptyContext(),
+    error: null,
+    invalidCharsRegex: /[^a-zA-Z0-9]/g, // only allow letters and numbers by default
+    isOpeningUpward: false,
+    maxVisibleOptions: 10, // max number of visible options
+    multipleSameSelections: true, // if true then same word can be selected multiple times
+    options: [],
+    sortAlphabetically: true, // options are sorted alphabetically by default
+    theme: null,
+    themeId: IDENTIFIERS.AUTOCOMPLETE,
+    themeOverrides: {}
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    // define refs
+    this.rootElement = createRef();
+    this.inputElement = createRef();
+    this.suggestionsElement = createRef();
+    this.optionsElement = createRef();
+
+    const {
+      context,
+      themeId,
+      theme,
+      themeOverrides,
+      sortAlphabetically,
+      options,
+      preselectedOptions
+    } = props;
+
+    this.state = {
+      inputValue: '',
+      error: '',
+      selectedOptions: preselectedOptions || [],
+      filteredOptions:
+        sortAlphabetically && options ? options.sort() : options || [],
+      isOpen: false,
+      composedTheme: composeTheme(
+        addThemeId(theme || context.theme, themeId),
+        addThemeId(themeOverrides, themeId),
+        context.ROOT_THEME_API
+      )
+    };
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    didThemePropsChange(this.props, nextProps, this.setState.bind(this));
+  }
+
+  clear = () => this._removeOptions();
+
+  focus = () => this.handleAutocompleteClick();
+
+  open = () => this.setState({ isOpen: true });
+
+  close = () => this.setState({ isOpen: false });
+
+  toggleOpen = () => this.setState({ isOpen: !this.state.isOpen });
+
+  handleAutocompleteClick = () => {
+    const { inputElement } = this;
+    if (inputElement && inputElement.current) {
+      inputElement.current.focus();
+    }
+    // toggle options open/closed
+    this.toggleOpen();
+  };
+
+  onKeyDown = (event: SyntheticKeyboardEvent<>) => {
+
+    if ( // Check for backspace in order to delete the last selected option
+      event.keyCode === 8 &&
+      !event.target.value &&
+      this.state.selectedOptions.length
+    ) {
+      // Remove last selected option
+      this.removeOption(this.state.selectedOptions.length - 1, event);
+    } else if (event.keyCode === 27) { // ESCAPE key: Stops propagation & modal closing
+      event.stopPropagation();
+    } else if (event.keyCode === 13) { // ENTER key: Opens suggestions
+      this.open();
+    }
+  };
+
+  // onChange handler for input element in AutocompleteSkin
+  handleInputChange = (event: SyntheticInputEvent<HTMLInputElement>) => {
+    this._setInputValue(event.target.value);
+  };
+
+  // passed to Options onChange handler in AutocompleteSkin
+  handleChange = (option: any, event: SyntheticEvent<>) => {
+    this.updateSelectedOptions(event, option);
+  };
+
+  updateSelectedOptions = (
+    event: SyntheticEvent<>,
+    selectedOption: any = null
+  ) => {
+    const { maxSelections, multipleSameSelections } = this.props;
+    const { selectedOptions, filteredOptions, isOpen } = this.state;
+    const canMoreOptionsBeSelected = (
+      maxSelections != null ? selectedOptions.length < maxSelections : true
+    );
+    const areFilteredOptionsAvailable = filteredOptions && filteredOptions.length > 0;
+
+    if (!maxSelections || (canMoreOptionsBeSelected && areFilteredOptionsAvailable)) {
+      if (!selectedOption) return;
+      const option = selectedOption.trim();
+      const optionCanBeSelected = (
+        (selectedOptions.indexOf(option) < 0 && !multipleSameSelections) ||
+        multipleSameSelections
+      );
+
+      if (option && optionCanBeSelected && isOpen) {
+        const newSelectedOptions = _.concat(selectedOptions, option);
+        this.selectionChanged(newSelectedOptions, event);
+        this.setState({ selectedOptions: newSelectedOptions, isOpen: false });
+      }
+    }
+
+    this._setInputValue('');
+  };
+
+  removeOption = (index: number, event: SyntheticEvent<>) => {
+    const selectedOptions = this.state.selectedOptions;
+    _.pullAt(selectedOptions, index);
+    this.selectionChanged(selectedOptions, event);
+    this.setState({ selectedOptions });
+  };
+
+  selectionChanged = (
+    selectedOptions: Array<any>,
+    event: SyntheticEvent<any>
+  ) => {
+    if (this.props.onChange) this.props.onChange(selectedOptions, event);
+  };
+
+  // returns an object containing props, theme, and method handlers
+  // associated with rendering this.state.selectedOptions, the user can call
+  // this in the body of the renderSelections function
+  getSelectionProps = ({
+    removeSelection
+  }: { removeSelection: Function } = {}) => {
+    const { themeId } = this.props;
+    const { inputValue, isOpen, selectedOptions, composedTheme } = this.state;
+    return {
+      inputValue,
+      isOpen,
+      selectedOptions,
+      theme: composedTheme[themeId],
+      removeSelection: (index: number, event: SyntheticEvent<>) =>
+        // the user's custom removeSelection event handler is composed with
+        // the internal functionality of Autocomplete (this.removeOption)
+        composeFunctions(removeSelection, this.removeOption)(index, event)
+    };
+  };
+
+  render() {
+    // destructuring props ensures only the "...rest" get passed down
+    const {
+      context,
+      invalidCharsRegex,
+      multipleSameSelections,
+      sortAlphabetically,
+      skin,
+      theme,
+      themeOverrides,
+      onChange,
+      error,
+      ...rest
+    } = this.props;
+
+    const AutocompleteSkin = skin || context.skins[IDENTIFIERS.AUTOCOMPLETE];
+
+    return (
+      <GlobalListeners
+        optionsIsOpen={this.state.isOpen}
+        optionsIsOpeningUpward={this.props.isOpeningUpward}
+        optionsRef={this.optionsElement}
+        rootRef={this.rootElement}
+        toggleOpen={this.toggleOpen}
+      >
+        {({ optionsMaxHeight }) => (
+          <AutocompleteSkin
+            error={error || this.state.error}
+            filteredOptions={this.state.filteredOptions}
+            getSelectionProps={this.getSelectionProps}
+            handleAutocompleteClick={this.handleAutocompleteClick}
+            handleChange={this.handleChange}
+            handleInputChange={this.handleInputChange}
+            inputRef={this.inputElement}
+            inputValue={this.state.inputValue}
+            isOpen={this.state.isOpen}
+            onKeyDown={this.onKeyDown}
+            optionsMaxHeight={optionsMaxHeight}
+            optionsRef={this.optionsElement}
+            removeOption={this.removeOption}
+            rootRef={this.rootElement}
+            selectedOptions={this.state.selectedOptions}
+            suggestionsRef={this.suggestionsElement}
+            theme={this.state.composedTheme}
+            toggleOpen={this.toggleOpen}
+            {...rest}
+          />
+        )}
+      </GlobalListeners>
+    );
+  }
+
+  // ======== PRIVATE METHOD ==========
+
+  _removeOptions = () => {
+    const { onChange } = this.props;
+    onChange ? onChange([]) : null;
+    this.setState({ selectedOptions: [], inputValue: '' });
+  };
+
+  _filterOptions = (value: string) => {
+    let filteredOptions = [];
+
+    if (value !== '') {
+      _.some(this.props.options, (option) => {
+        if (_.startsWith(option, value)) {
+          filteredOptions.push(option);
+        }
+      });
+    } else {
+      filteredOptions = this.props.options;
+    }
+
+    return filteredOptions;
+  };
+
+  _filterInvalidChars = (value: string) => {
+    let filteredValue = '';
+
+    if (this.props.invalidCharsRegex.test(value)) {
+      filteredValue = value.replace(this.props.invalidCharsRegex, '');
+    } else {
+      filteredValue = value;
+    }
+
+    return filteredValue;
+  };
+
+  _setInputValue = (value: string) => {
+    const filteredValue = this._filterInvalidChars(value);
+    const filteredOptions = this._filterOptions(filteredValue);
+    this.setState({
+      isOpen: true,
+      inputValue: filteredValue,
+      filteredOptions
+    });
+  }
+}
+
+export const Autocomplete = withTheme(AutocompleteBase);
---
+++ source/components/Bubble.js
@@ -0,0 +1,207 @@
+// @flow
+import React, { Component } from 'react';
+import type { ComponentType, Element, ElementRef } from 'react';
+import createRef from 'create-react-ref/lib/createRef';
+
+// internal utility functions
+import { createEmptyContext, withTheme } from './HOC/withTheme';
+import { composeTheme, addThemeId, didThemePropsChange } from '../utils/themes';
+import { addDocumentListeners, removeDocumentListeners } from '../utils/events';
+
+// import constants
+import { IDENTIFIERS } from '.';
+import type { ThemeContextProp } from './HOC/withTheme';
+
+type Props = {
+  className?: string,
+  context: ThemeContextProp,
+  isHidden: boolean,
+  isFloating: boolean,
+  isOpeningUpward: boolean,
+  isTransparent: boolean,
+  skin?: ComponentType<any>,
+  theme: ?Object, // takes precedence over them in context if passed
+  themeId: string,
+  themeOverrides: Object, // custom css/scss from user adhering to component's theme API
+  targetRef?: ElementRef<*>, // ref to the target DOM element used for positioning the bubble
+};
+
+type State = {
+  composedTheme: Object,
+  position: ?Object
+};
+
+class BubbleBase extends Component<Props, State> {
+  // declare ref types
+  rootElement: ?Element<any>;
+
+  // define static properties
+  static displayName = 'Bubble';
+  static defaultProps = {
+    context: createEmptyContext(),
+    isHidden: false,
+    isFloating: false,
+    isOpeningUpward: false,
+    isTransparent: true,
+    theme: null,
+    themeId: IDENTIFIERS.BUBBLE,
+    themeOverrides: {}
+  };
+
+  constructor(props: Props) {
+    super(props);
+
+    // define ref
+    this.rootElement = createRef();
+
+    const { context, themeId, theme, themeOverrides } = props;
+
+    this.state = {
+      composedTheme: composeTheme(
+        addThemeId(theme || context.theme, themeId),
+        addThemeId(themeOverrides, themeId),
+        context.ROOT_THEME_API
+      ),
+      position: null
+    };
+  }
+
+  _hasEventListeners = false;
+
+  componentDidMount() {
+    setTimeout(() => {
+      if (this.props.isFloating) this._updatePosition();
+    }, 0);
+  }
+
+  componentWillReceiveProps(nextProps: Props) {
+    didThemePropsChange(this.props, nextProps, this.setState.bind(this));
+  }
+
+  componentWillUpdate(nextProps) {
+    const { isFloating } = this.props;
+    // Add listeners when the bubble
+    if (isFloating && !nextProps.isHidden && !this._hasEventListeners) {
+      this._handleScrollEventListener('add');
+      addDocumentListeners(this._getDocumentEvents());
+      window.addEventListener('resize', this._updatePosition);
+      this._hasEventListeners = true;
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const { isHidden } = this.props;
+    const didVisibilityChange = isHidden !== prevProps.isHidden;
+    const wasBubbleHidden = !prevProps.isHidden && isHidden;
+
+    if (wasBubbleHidden) this._removeAllEventListeners();
+    if (didVisibilityChange) this._updatePosition();
+  }
+
+  componentWillUnmount() {
+    if (this._hasEventListeners) this._removeAllEventListeners();
+  }
+
+  // =========== PRIVATE HELPERS ==============
+
+  _handleScrollEventListener = (action: string) => {
+    // const rootNode = this.rootElement;
+    const { rootElement } = this;
+    if (rootElement) {
+      const scrollableNode = this._getFirstScrollableParent(rootElement);
+      if (scrollableNode) {
+        if (action === 'add') {
+          scrollableNode.addEventListener('scroll', this._updatePosition);
+        } else if (action === 'remove') {
+          scrollableNode.removeEventListener('scroll', this._updatePosition);
+        }
+      }
+    }
+  };
+
+  _removeAllEventListeners() {
+    if (this._hasEventListeners) {
+      removeDocumentListeners(this._getDocumentEvents());
+      this._handleScrollEventListener('remove');
+      window.removeEventListener('resize', this._updatePosition);
+      this._hasEventListeners = false;
+    }
+  }
+
+  _getFirstScrollableParent = (element: ElementRef<*>) => {
+    if (element == null) return null;
+    const { rootElement } = this;
+    const node = {}.hasOwnProperty.call(element, 'current') ? element.current : element;
+
+    if (rootElement) {
+      if (node === rootElement.current || node.scrollHeight <= node.clientHeight) {
+        return this._getFirstScrollableParent(node.parentElement);
+      }
+    }
+
+    return node;
+  };
+
+  _updatePosition = () => {
+    const { isOpeningUpward, targetRef } = this.props;
+    const { rootElement } = this;
+
+    let target = targetRef && typeof targetRef !== 'string' ? targetRef.current : null;
+
+    // Without a target, try to fallback to the parent node
+    if (!target) {
+      //  Only proceed if the root element is defined
+      if (!rootElement || !rootElement.current) return;
+      target = rootElement.current.parentElement;
+    }
+
+    const targetRect = target.getBoundingClientRect();
+
+    let positionY;
+    if (isOpeningUpward) {
+      // Since we don't know the height of the bubble before rendering it we positioning
+      // it with { bottom: XYpx } (within the viewport) and need this calculation:
+      positionY = window.innerHeight - targetRect.top + 20;
+    } else {
+      positionY = targetRect.bottom + 20;
+    }
+
+    const position = {
+      width: targetRect.width,
+      positionX: targetRect.left,
+      positionY
+    };
+    this.setState({ position });
+  };
+
+  _getDocumentEvents() {
+    return {
+      resize: this._updatePosition,
+      scroll: this._updatePosition
+    };
+  }
+
+  render() {
+    // destructuring props ensures only the "...rest" get passed down
+    const {
+      skin,
+      theme,
+      themeOverrides,
+      context,
+      ...rest
+    } = this.props;
+
+    const BubbleSkin = skin || context.skins[IDENTIFIERS.BUBBLE];
+
+    return (
+      <BubbleSkin
+        rootRef={this.rootElement}
+        position={this.state.position}
+        theme={this.state.composedTheme}
+        {...rest}
+      />
+    );
+  }
+}
+
+export const Bubble = withTheme(BubbleBase);
